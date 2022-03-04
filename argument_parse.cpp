f72d1504d869e070c41d90c8b02d6e8e302871e8
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-04 12:22:59 +0100

    Implementation od argumetn parse.

--- argument_parse.cpp
+++ argument_parse.cpp
@@ -1,15 +1,48 @@
 #include "argument_parse.h"
 
-#include <getopt.h>
-#include <string>
-
-int read_arguments(arguments &arg) {
+int read_arguments(int argc, char **argv, arguments &arg) {
   struct option long_options[] = {
       {"source", required_argument, nullptr, 's'},
-      {"destination", optional_argument, nullptr, 'd'},
-      {"threats", optional_argument, nullptr, 't'},
+      {"destination", required_argument, nullptr, 'd'},
+      {"threads", required_argument, nullptr, 't'},
       {"quiet", no_argument, nullptr, 'q'},
       {nullptr, 0, nullptr, 0}};
 
-  return 1;
+  char c;
+  std::string path;
+  while ((c = getopt_long(argc, argv, "s:d:t:q", long_options, NULL)) != -1) {
+    switch (c) {
+    case 's':
+      arg.source = parse_to_path(optarg);
+      break;
+    case 'd':
+      arg.destination = parse_to_path(optarg);
+      break;
+    case 't':
+      arg.threads = parse_to_unsigned(optarg);
+      break;
+    case 'q':
+      arg.quiet = true;
+    default:
+      break;
+    }
+  }
+
+  return 0;
+}
+
+std::filesystem::path parse_to_path(const std::string &path) {
+  std::filesystem::path p(path);
+  if (!std::filesystem::exists(p))
+    throw std::invalid_argument(path + " does not exist.");
+  if (!std::filesystem::is_directory(p))
+    throw std::invalid_argument(path + "is not directory.");
+  return p;
+}
+
+unsigned int parse_to_unsigned(const std::string & number) {
+  int threads = std::stoi(number);
+  if (threads <= 0)
+    throw std::invalid_argument("Number of threads must be positive number.");
+  return (unsigned int) threads;
 }
--- argument_parse.h
+++ argument_parse.h
@@ -1,11 +1,16 @@
+#include <filesystem>
 #include <getopt.h>
 #include <string>
 
 struct arguments {
-  std::string source;
-  std::string destination;
-  size_t threats;
-  bool quiet;
+  std::filesystem::path source;
+  std::filesystem::path destination;
+  unsigned int threads = 1;
+  bool quiet = false;
 };
 
-int read_arguments(arguments &);
+int read_arguments(int, char **, arguments &);
+
+std::filesystem::path parse_to_path(const std::string &);
+
+unsigned int parse_to_unsigned(const std::string &);

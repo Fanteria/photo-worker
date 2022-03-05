b7cc823b929f52bb41a899497993e367fbf07783
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-05 13:51:51 +0100

    Change arg from pointer to value.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@ int process_image(const std::string &file, LibRaw &iProcessor);
 void print_image_info(const LibRaw &iProcessor);
 
 int main(int argc, char **args) {
-  arguments *arg = new arguments();
+  arguments arg = arguments();
   try {
-    read_arguments(argc, args, *arg);
+    read_arguments(argc, args, arg);
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
     exit(1);
   }
 
   LibRaw iProcessor;
-  Convertor convertor = Convertor(arg->source, arg->destination);
+  Convertor convertor = Convertor(arg.source, arg.destination);
  
   convertor.convert_photo("example.CR2", iProcessor);
 

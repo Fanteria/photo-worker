bf7ae08d70016e5648c4818b0fa3bce38beaf46a
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-05 16:38:09 +0100

    Add values and pront for them.

--- picture.cpp
+++ picture.cpp
@@ -1,1 +1,49 @@
 #include "picture.h"
+
+std::pair<unsigned int, unsigned int> Picture::get_image_size() {
+    return std::make_pair(this->width, this->height);
+}
+
+std::pair<unsigned int, unsigned int> Picture::get_output_image_size() {
+    return std::make_pair(this->out_width, this->out_height);
+}
+
+std::pair<unsigned int, unsigned int> Picture::get_margin_size() {
+    return std::make_pair(this->top_margin, this->left_margin);
+}
+
+std::pair<unsigned int, unsigned int> Picture::get_raw_image_size() {
+    return std::make_pair(this->raw_width, this->raw_height);
+}
+
+std::ostream& operator<< (std::ostream & os, const Picture & pic) {
+    std::cout << "Image size:\t";
+    std::cout << pic.width;
+    std::cout << " x ";
+    std::cout << pic.height;
+    std::cout << "\t\tWithout frame";
+    std::cout << std::endl;
+
+    std::cout << "Output image:\t";
+    std::cout << pic.out_width;
+    std::cout << " x ";
+    std::cout << pic.out_height;
+    std::cout << "\t\tSize of output image, can be different";
+    std::cout << std::endl;
+
+    std::cout << "Margin size:\t";
+    std::cout << pic.top_margin;
+    std::cout << " x ";
+    std::cout << pic.left_margin;
+    std::cout << "\t\tCoordinates of left top corner shift";
+    std::cout << std::endl;
+
+    std::cout << "Raw image size:\t";
+    std::cout << pic.raw_width;
+    std::cout << " x ";
+    std::cout << pic.raw_height;
+    std::cout << "\t\tWith frame";
+    std::cout << std::endl;
+
+    return os;
+}
--- picture.h
+++ picture.h
@@ -1,12 +1,50 @@
 #include <filesystem>
 #include <string>
+#include <iostream>
 
 class Picture {
-    std::filesystem::path raw;
-    std::filesystem::path converted;
-    std::string name;
-    unsigned int width;
-    unsigned int height;
+private:
+    // Image size without frame
+    const unsigned int width;
+    const unsigned int height;
 
-    Picture(const std::string & name) : name(name) {}
+    // Output image size
+    const unsigned int out_width;
+    const unsigned int out_height;
+
+    // Margin size for shift from left corner
+    const unsigned int top_margin;
+    const unsigned int left_margin;
+
+    // Raw size with frame
+    const unsigned int raw_width;
+    const unsigned int raw_height;
+
+public:
+    std::string name;
+
+    Picture(const std::string & name,
+            const unsigned int width,
+            const unsigned int height,
+            const unsigned int out_width,
+            const unsigned int out_height,
+            const unsigned int top_margin,
+            const unsigned int left_margin,
+            const unsigned int raw_width,
+            const unsigned int raw_height) : 
+            width(width), height(height),
+            out_width(out_width), out_height(out_height),
+            top_margin(top_margin), left_margin(left_margin),
+            raw_width(raw_width), raw_height(raw_height),
+            name(name) {}
+
+    std::pair<unsigned int, unsigned int> get_image_size();
+
+    std::pair<unsigned int, unsigned int> get_output_image_size();
+
+    std::pair<unsigned int, unsigned int> get_margin_size();
+
+    std::pair<unsigned int, unsigned int> get_raw_image_size();
+
+    friend std::ostream& operator<< (std::ostream &, const Picture &);
 };

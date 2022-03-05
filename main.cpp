fe031282ac1474091ccfd8b65b84019a731c23e2
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-05 10:00:55 +0100

    Use Convertor in main.

--- main.cpp
+++ main.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
-#include <string>
 #include <libraw/libraw.h>
+#include <string>
 
-int process_image(const std::string & file, LibRaw & iProcessor);
-void print_image_info(const LibRaw & iProcessor);
+#include "argument_parse.h"
+#include "convertor.h"
 
-int main(int argc, char ** args) {
-    LibRaw iProcessor;
-    process_image("example.CR2", iProcessor);
-    return 0;
-}
+int process_image(const std::string &file, LibRaw &iProcessor);
+void print_image_info(const LibRaw &iProcessor);
 
-int process_image(const std::string & file, LibRaw & iProcessor) {
-    int ret;
-    if ((ret = iProcessor.open_file(file.c_str())) != LIBRAW_SUCCESS) {
-        std::cerr << "Error: " << libraw_strerror(ret) << std::endl;
-        return ret;
-    }
+int main(int argc, char **args) {
+  arguments *arg = new arguments();
+  try {
+    read_arguments(argc, args, *arg);
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+    exit(1);
+  }
 
-    iProcessor.unpack();
-    
-    print_image_info(iProcessor);
+  LibRaw iProcessor;
+  Convertor convertor = Convertor(arg->source, arg->destination);
+ 
+  convertor.convert_photo("example.CR2", iProcessor);
 
-    return 0; 
-     
-}
-
-void print_image_info(const LibRaw & iProcessor) {
-    std::cout << "Image size:\t";
-    std::cout << iProcessor.imgdata.sizes.width;
-    std::cout << " x ";
-    std::cout << iProcessor.imgdata.sizes.height;
-    std::cout << "\t\tWithout frame";
-    std::cout << std::endl;
-
-    std::cout << "Raw image size:\t";
-    std::cout << iProcessor.imgdata.sizes.raw_width;
-    std::cout << " x ";
-    std::cout << iProcessor.imgdata.sizes.raw_height;
-    std::cout << "\t\tWith frame";
-    std::cout << std::endl;
+  return EXIT_SUCCESS;
 }

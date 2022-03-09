9a9173e2b28913bb05fd1712ff34880288cacf20
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-09 10:58:06 +0100

    Remove unused filesystem include.

--- src/pictureData.cpp
+++ src/pictureData.cpp
@@ -1,49 +1,49 @@
 #include "pictureData.hpp"
 
 std::pair<unsigned int, unsigned int> PictureData::get_image_size() {
-    return std::make_pair(this->width, this->height);
+  return std::make_pair(this->width, this->height);
 }
 
 std::pair<unsigned int, unsigned int> PictureData::get_output_image_size() {
-    return std::make_pair(this->out_width, this->out_height);
+  return std::make_pair(this->out_width, this->out_height);
 }
 
 std::pair<unsigned int, unsigned int> PictureData::get_margin_size() {
-    return std::make_pair(this->top_margin, this->left_margin);
+  return std::make_pair(this->top_margin, this->left_margin);
 }
 
 std::pair<unsigned int, unsigned int> PictureData::get_raw_image_size() {
-    return std::make_pair(this->raw_width, this->raw_height);
+  return std::make_pair(this->raw_width, this->raw_height);
 }
 
-std::ostream& operator<< (std::ostream & os, const PictureData & pic) {
-    os << "Image size:\t";
-    os << pic.width;
-    os << " x ";
-    os << pic.height;
-    os << "\t\tWithout frame";
-    os << std::endl;
+std::ostream &operator<<(std::ostream &os, const PictureData &pic) {
+  os << "Image size:\t";
+  os << pic.width;
+  os << " x ";
+  os << pic.height;
+  os << "\t\tWithout frame";
+  os << std::endl;
 
-    os << "Output image:\t";
-    os << pic.out_width;
-    os << " x ";
-    os << pic.out_height;
-    os << "\t\tSize of output image, can be different";
-    os << std::endl;
+  os << "Output image:\t";
+  os << pic.out_width;
+  os << " x ";
+  os << pic.out_height;
+  os << "\t\tSize of output image, can be different";
+  os << std::endl;
 
-    os << "Margin size:\t";
-    os << pic.top_margin;
-    os << " x ";
-    os << pic.left_margin;
-    os << "\t\tCoordinates of left top corner shift";
-    os << std::endl;
+  os << "Margin size:\t";
+  os << pic.top_margin;
+  os << " x ";
+  os << pic.left_margin;
+  os << "\t\tCoordinates of left top corner shift";
+  os << std::endl;
 
-    os << "Raw image size:\t";
-    os << pic.raw_width;
-    os << " x ";
-    os << pic.raw_height;
-    os << "\t\tWith frame";
-    os << std::endl;
+  os << "Raw image size:\t";
+  os << pic.raw_width;
+  os << " x ";
+  os << pic.raw_height;
+  os << "\t\tWith frame";
+  os << std::endl;
 
-    return os;
+  return os;
 }
--- src/pictureData.hpp
+++ src/pictureData.hpp
@@ -1,51 +1,46 @@
 #ifndef PICTURE_DATA_H_INCLUDED
 #define PICTURE_DATA_H_INCLUDED
 
-#include <filesystem>
-#include <string>
 #include <iostream>
+#include <string>
 
 class PictureData {
 private:
-    // Image size without frame
-    const unsigned int width;
-    const unsigned int height;
+  // Image size without frame
+  const unsigned int width;
+  const unsigned int height;
 
-    // Output image size
-    const unsigned int out_width;
-    const unsigned int out_height;
+  // Output image size
+  const unsigned int out_width;
+  const unsigned int out_height;
 
-    // Margin size for shift from left corner
-    const unsigned int top_margin;
-    const unsigned int left_margin;
+  // Margin size for shift from left corner
+  const unsigned int top_margin;
+  const unsigned int left_margin;
 
-    // Raw size with frame
-    const unsigned int raw_width;
-    const unsigned int raw_height;
+  // Raw size with frame
+  const unsigned int raw_width;
+  const unsigned int raw_height;
 
 public:
-    PictureData(const unsigned int width,
-            const unsigned int height,
-            const unsigned int out_width,
-            const unsigned int out_height,
-            const unsigned int top_margin,
-            const unsigned int left_margin,
-            const unsigned int raw_width,
-            const unsigned int raw_height) : 
-            width(width), height(height),
-            out_width(out_width), out_height(out_height),
-            top_margin(top_margin), left_margin(left_margin),
-            raw_width(raw_width), raw_height(raw_height) {}
+  PictureData(const unsigned int width, const unsigned int height,
+              const unsigned int out_width, const unsigned int out_height,
+              const unsigned int top_margin, const unsigned int left_margin,
+              const unsigned int raw_width, const unsigned int raw_height)
+      : width(width), height(height), out_width(out_width),
+        out_height(out_height), top_margin(top_margin),
+        left_margin(left_margin), raw_width(raw_width), raw_height(raw_height) {
+  }
 
-    std::pair<unsigned int, unsigned int> get_image_size();
+  std::pair<unsigned int, unsigned int> get_image_size();
 
-    std::pair<unsigned int, unsigned int> get_output_image_size();
+  std::pair<unsigned int, unsigned int> get_output_image_size();
 
-    std::pair<unsigned int, unsigned int> get_margin_size();
+  std::pair<unsigned int, unsigned int> get_margin_size();
 
-    std::pair<unsigned int, unsigned int> get_raw_image_size();
+  std::pair<unsigned int, unsigned int> get_raw_image_size();
 
-    friend std::ostream& operator<< (std::ostream &, const PictureData &);
+  friend std::ostream &operator<<(std::ostream &, const PictureData &);
 };
 
 #endif // PICTURE_DATA_H_INCLUDED

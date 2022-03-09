f35bd1b75577c6f0167095cfc10607d070aacdf9
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-09 17:10:53 +0100

    Create method for syncing folders.

--- src/worker.cpp
+++ src/worker.cpp
@@ -9,17 +9,33 @@ inline fs::path Worker::new_path(const fs::path &path,
 }
 
 inline std::set<fs::path> Worker::get_sorted_files(const fs::path &path,
-                                                   const std::string &suffix) {
+                                                   const std::string *suffix) {
   std::set<fs::path> sorted_files;
   for (const auto &file : fs::directory_iterator(path)) {
-    if (file.path().extension() == suffix) {
-      sorted_files.insert(file.path());
-    }
+    if (suffix && file.path().extension() != *suffix)
+      continue;
+    sorted_files.insert(file.path());
   }
   return sorted_files;
 }
 
-void Worker::sync_photos(work_type type) {}
+void Worker::remove_surplus_files(const fs::path &original,
+                                  const fs::path &synced) {
+  std::map<std::string, fs::path> files_set;
+  for (const auto &file : fs::directory_iterator(synced)) {
+    if (file.is_regular_file())
+      files_set.insert(
+          std::pair<std::string, fs::path>(get_filename(file), file));
+  }
+  for (const auto &file : fs::directory_iterator(original)) {
+    files_set.erase(get_filename(file));
+  }
+  for (const auto &file : files_set) {
+    fs::remove(file.second);
+  }
+}
+
+void Worker::sync_photos(work_type type) { remove_surplus_files(src, dest); }
 
 void Worker::rename_photos(const std::string &name, work_type type) {
   switch (type) {
@@ -56,6 +72,11 @@ void Worker::read_converted_files(std::vector<std::string> &files) {
   Worker::read_files(files, converted_suffix, dest);
 }
 
+inline std::string Worker::get_filename(const fs::path &path) {
+  std::string file_name = std::string(path.filename());
+  return file_name.substr(0, file_name.find_last_of('.'));
+}
+
 void Worker::read_files(std::vector<std::string> &files,
                         const std::string &suffix, const fs::path &path) {
   for (const auto &entry : fs::directory_iterator(path)) {
@@ -68,7 +89,7 @@ void Worker::rename_files_in_folder(const fs::path &path,
                                     const std::string &name,
                                     const std::string &suffix, bool renumber,
                                     bool verbose, bool ask) {
-  std::set<fs::path> sorted_files = Worker::get_sorted_files(path, suffix);
+  std::set<fs::path> sorted_files = Worker::get_sorted_files(path, &suffix);
   if (renumber) {
     int number = 1;
     for (auto const &file : sorted_files) {
--- src/worker.hpp
+++ src/worker.hpp
@@ -2,6 +2,7 @@
 #define WORKER_H_INCLUDED
 
 #include <filesystem>
+#include <map>
 #include <set>
 #include <string>
 #include <vector>
@@ -17,7 +18,8 @@ private:
 
   static fs::path new_path(const fs::path &, const std::string &);
   static std::set<fs::path> get_sorted_files(const fs::path &,
-                                             const std::string &);
+                                             const std::string *);
+  static void remove_surplus_files(const fs::path &, const fs::path &);
 
 public:
   std::string raw_suffix = ".CR2";
@@ -36,6 +38,8 @@ public:
   void read_raw_files(std::vector<std::string> &);
 
   void read_converted_files(std::vector<std::string> &);
+
+  static std::string get_filename(const fs::path &);
 
   static void read_files(std::vector<std::string> &, const std::string &,
                          const fs::path &);

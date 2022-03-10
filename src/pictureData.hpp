f6a5efc0c7016ac9b24a694d249d93a8abd4bda5
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-10 12:58:03 +0100

    Doxygen documentation for class PictureData.

--- src/pictureData.hpp
+++ src/pictureData.hpp
@@ -23,6 +23,18 @@ private:
   const unsigned int raw_height;
 
 public:
+  /**
+   * @brief Construct a new Picture Data object.
+   *
+   * @param width image width without frame
+   * @param height image height without frame
+   * @param out_width output image width
+   * @param out_height output image height
+   * @param top_margin margin size on top of image
+   * @param left_margin margin size of left of image
+   * @param raw_width raw image width
+   * @param raw_height raw image height
+   */
   PictureData(const unsigned int width, const unsigned int height,
               const unsigned int out_width, const unsigned int out_height,
               const unsigned int top_margin, const unsigned int left_margin,
@@ -32,15 +44,42 @@ public:
         left_margin(left_margin), raw_width(raw_width), raw_height(raw_height) {
   }
 
+  /**
+   * @brief Get size of the image.
+   *
+   * @return std::pair<unsigned int, unsigned int> size of image as <width,
+   * height>
+   */
   std::pair<unsigned int, unsigned int> get_image_size();
 
+  /**
+   * @brief Get size of the output image.
+   *
+   * @return std::pair<unsigned int, unsigned int> size of output image as
+   * <width, height>
+   */
   std::pair<unsigned int, unsigned int> get_output_image_size();
 
+  /**
+   * @brief Get the margin size of the picture.
+   *
+   * @return std::pair<unsigned int, unsigned int> size of margin of the picture
+   * as <top, left>
+   */
   std::pair<unsigned int, unsigned int> get_margin_size();
 
+  /**
+   * @brief Get the size of raw image.
+   *
+   * @return std::pair<unsigned int, unsigned int> size of the raw image ad
+   * <width, height>
+   */
   std::pair<unsigned int, unsigned int> get_raw_image_size();
 
-  friend std::ostream &operator<<(std::ostream &, const PictureData &);
+  /**
+   * @brief Overload operator <<.
+   */
+  friend std::ostream &operator<<(std::ostream &os, const PictureData &pic);
 };
 
 #endif // PICTURE_DATA_H_INCLUDED

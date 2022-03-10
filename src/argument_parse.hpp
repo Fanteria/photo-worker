7a83ab8464a7bb2d00f4ae13a9b1ddafe0fb96be
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-10 13:52:21 +0100

    Write Doxygen documentation for argument parse.

--- src/argument_parse.hpp
+++ src/argument_parse.hpp
@@ -15,9 +15,25 @@ struct arguments {
   std::string name = "";
 };
 
-int read_arguments(int, char **, arguments &);
+/**
+ * @brief Read arguments and parse them to struct.
+ *
+ * @param argc is argument count
+ * @param argv is argument vector
+ * @param arg reference to structure for parse flags
+ * @return int if 1, then arguments was read right
+ */
+int read_arguments(int argc, char **argv, arguments &arg);
 
-std::filesystem::path parse_to_path(const std::string &,
-                                    bool must_exist = true);
+/**
+ * @brief Parse string to path and check if it exist, if not, and exist parent,
+ * can be created depends on argument setup. If folder not exist (and
+ * have not parent), then exception is thrown.
+ *
+ * @param path is string representation of path
+ * @param must_exist if it is true, can create folder, if exist parent
+ * @return std::filesystem::path created path
+ */
+fs::path parse_to_path(const std::string &path, bool must_exist = true);
 
-unsigned int parse_to_unsigned(const std::string &);
+unsigned int parse_to_unsigned(const std::string &number);

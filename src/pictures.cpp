0105c9cb466da71382a410a5ba4378d273bd6e0e
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-03-17 09:44:06 +0100

    Add using to .cpp file.

--- src/pictures.cpp
+++ src/pictures.cpp
@@ -1,20 +1,25 @@
 #include "pictures.hpp"
+#include <pstl/glue_execution_defs.h>
+#include <vector>
+
+using std::for_each;
+using std::string;
+using std::vector;
+using std::execution::par_unseq;
 
 Pictures::~Pictures() {
-  std::for_each(std::execution::par_unseq, pics.begin(), pics.end(),
-                [](auto &pic) { delete pic.second; });
+  for_each(par_unseq, pics.begin(), pics.end(),
+           [](auto &pic) { delete pic.second; });
 }
 
-void Pictures::addPicture(const std::string &name, PictureData *pic) {
+void Pictures::addPicture(const string &name, PictureData *pic) {
   pics.insert(make_pair(name, pic));
 }
 
-PictureData *Pictures::get_pic_data(const std::string &name) {
-  return pics[name];
-}
+PictureData *Pictures::get_pic_data(const string &name) { return pics[name]; }
 
-std::vector<std::string> Pictures::listAll() {
-  std::vector<std::string> ret;
+vector<string> Pictures::listAll() {
+  vector<string> ret;
   for (auto const &i : pics) {
     ret.push_back(i.first);
   }
